a31626c28deee797303e5611858a1cb9482adc96
Joseph <aacejovi@gmail.com>
2024-06-29 22:46:35 +0100

    Fist button done

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 let count = 0;
 let idOne = document.getElementById("input-one");
 
+/* Home function*/
+
 function increment() {
     count++
     idOne.textContent = count;
 }
+
+function plustwo() {
+    count+= 2;
+    idOne.textContent = count;
+}
+
+function Clear() {
+    count= 0;
+    idOne.textContent = count;
+}
+
+function plusthree(){
+    count+= 3;
+    idOne.textContent = count;
+}
+
+/*  Away function */
